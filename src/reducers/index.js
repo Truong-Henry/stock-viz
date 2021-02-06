8ddcdf7baa48c6bbe2aa7c24fc97e30311d5297d
Henry Truong <HenryTruong1997@gmail.com>
2021-02-06 18:19:59 -0500

    Added newsReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import newsReducer from "./newsReducer";
 import searchReducer from "./searchReducer";
 import stockReducer from "./stockReducer";
 
 export default combineReducers({
   search: searchReducer,
   stock: stockReducer,
+  news: newsReducer,
 });
